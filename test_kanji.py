c1037758cfbb491025d0bb63a70e5d2c8fdae771
teftef <hanying0226tohoku@gmail.com>
2025-04-02 22:39:52 +0900

    Move Model Constructor

--- test_kanji.py
+++ test_kanji.py
@@ -13,6 +13,8 @@ from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 import random
 
+from networks.My_unet import My_Unet,CombinedModel,Time_Embed,Cond_Embed
+
 # parser
 parser = ArgumentParser()
 parser.add_argument("--optimizer", type=str, default="RAdamScheduleFree", help="optimizer")
@@ -28,62 +30,6 @@ parser.add_argument("--use_label_clip_embedding",type=bool,default=True,help="us
 args = parser.parse_args()
 
 
-class My_Unet(nn.Module):
-    def __init__(self, in_channels, embedding_channels=64, time_embed_dim=256, cond_embed_dim=256, depth=4):
-        super(My_Unet, self).__init__()
-
-        self.unet = UNet(
-            in_channels=in_channels,
-            embedding_channels=embedding_channels,
-            cond_embed_dim=cond_embed_dim,
-            time_embed_dim=time_embed_dim,
-            depth=depth,
-            kernel_size=[3,3,3,3,3,3,3],
-            layers=[3,3,3,9,3,3,3],
-            num_groups=[32] * (depth * 2 - 1) 
-        )
-    
-    def forward(self, x, t, c):
-        return self.unet(x, t, c)
-    
-class Time_Embed(nn.Module):
-    def __init__(self, time_embed_dim=256):
-        super(Time_Embed, self).__init__()
-        self.time_embed = nn.Linear(1,time_embed_dim)
-        self.reg = nn.ReLU(inplace=False)
-    
-    def forward(self, t):
-        return self.reg(self.time_embed(t))
-
-class Cond_Embed(nn.Module):
-    def __init__(self,label_num=10, cond_embed_dim=256):
-        super(Cond_Embed, self).__init__()
-        self.hidden_layer = nn.Linear(label_num, 1024)
-        self.cond_embed = nn.Linear(1024,cond_embed_dim)
-        # self.normalize = nn.LayerNorm(cond_embed_dim)
-        self.reg = nn.ReLU(inplace=False)
-    
-    def forward(self, c):
-        c = self.hidden_layer(c)
-        c = self.reg(c)
-        out = self.cond_embed(c)
-        # out = self.normalize(out)
-        return out
-
-
-class CombinedModel(nn.Module):
-    def __init__(self, unet, time_embed, cond_embed):
-        super(CombinedModel, self).__init__()
-        self.unet = unet
-        self.time_embed = time_embed
-        self.cond_embed = cond_embed
-
-    def forward(self, x, t, c):
-        t = self.time_embed(t)
-        c = self.cond_embed(c)
-        return self.unet(x, t, c)
-
-
 if __name__ == "__main__":
 
     if args.seed != -1:
@@ -93,9 +39,6 @@ if __name__ == "__main__":
 
     # GPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # model
-    # unet = My_Unet(in_channels=1, embedding_channels=64).to(device)
-    # time_embed = Time_Embed().to(device)
 
     if args.use_label_clip_embedding:
         label_num = 512
--- train_kanji_rectified_flow.py
+++ train_kanji_rectified_flow.py
@@ -17,11 +17,8 @@ from dotenv import load_dotenv
 from torch.utils.data import Dataset
 import json
 import random
+from networks.My_unet import My_Unet,CombinedModel,Time_Embed,Cond_Embed
 
-
-
-# from DiT_GumbelSoftmax.network.dit import DiffusionTransformer
-# from DiT_GumbelSoftmax.config import Config
 
 # parser
 parser = ArgumentParser()
@@ -48,60 +45,6 @@ else:
 
 # GPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-class My_Unet(nn.Module):
-    def __init__(self, in_channels, embedding_channels=64, time_embed_dim=256, cond_embed_dim=256, depth=4):
-        super(My_Unet, self).__init__()
-
-        self.unet = UNet(
-            in_channels=in_channels,
-            embedding_channels=embedding_channels,
-            cond_embed_dim=cond_embed_dim,
-            time_embed_dim=time_embed_dim,
-            depth=depth,
-            kernel_size=[3,3,3,3,3,3,3],
-            layers=[3,3,3,9,3,3,3],
-            num_groups=[32] * (depth * 2 - 1) 
-        )
-    
-    def forward(self, x, t, c):
-        return self.unet(x, t, c)
-    
-class Time_Embed(nn.Module):
-    def __init__(self, time_embed_dim=256):
-        super(Time_Embed, self).__init__()
-        self.time_embed = nn.Linear(1,time_embed_dim)
-        self.reg = nn.ReLU(inplace=False)
-    
-    def forward(self, t):
-        return self.reg(self.time_embed(t))
-
-class Cond_Embed(nn.Module):
-    def __init__(self,label_num=10, cond_embed_dim=256):
-        super(Cond_Embed, self).__init__()
-        self.hidden_layer = nn.Linear(label_num, 1024)
-        self.cond_embed = nn.Linear(1024,cond_embed_dim)
-        # self.normalize = nn.LayerNorm(cond_embed_dim)
-        self.reg = nn.ReLU(inplace=False)
-    
-    def forward(self, c):
-        c = self.hidden_layer(c)
-        c = self.reg(c)
-        out = self.cond_embed(c)
-        # out = self.normalize(out)
-        return out
-
-class CombinedModel(nn.Module):
-    def __init__(self, unet, time_embed, cond_embed):
-        super(CombinedModel, self).__init__()
-        self.unet = unet
-        self.time_embed = time_embed
-        self.cond_embed = cond_embed
-
-    def forward(self, x, t, c):
-        t = self.time_embed(t)
-        c = self.cond_embed(c)
-        return self.unet(x, t, c)
 
 # model
 if args.dataset == "Kanji":
@@ -188,8 +131,7 @@ for epoch in iterator:
     for i, (images, labels) in enumerate(train_loader):
         labels = labels.to(device)
         images = images.to(device) #(batch, 1, 28, 28)
-
-        # labels = torch.nn.functional.one_hot(labels, data_label_num).float().to(device)
+        
 
         ## ドロップアウト　if args.train_use_cfg
         if args.train_use_cfg:
